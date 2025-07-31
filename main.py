4a0e5796ef6a942b0a10e538896cb56f4b2ee865
MarcoGarzonJimenez <mj600888@gmail.com>
2025-07-30 21:26:43 -0500

    se descargaron las stopwords en el disco local c

---
+++ main.py
@@ -0,0 +1,10 @@
+# importar la libreria nltk
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# definir la ruta donde se almacenaran los datos descargados de nltk
+nltk.data.path.append('C:/Users/STIVEN/AppData/Roaming/nltk_data')
+
+
